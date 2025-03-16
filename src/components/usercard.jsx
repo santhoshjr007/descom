925a3f32462f63797dcc9c1da33da0bed8af2ca9
santhoshjr007 <santhosh.r@kalvium.community>
2025-03-17 02:11:15 +0530

    done

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,20 @@
 // write the component code her
+import React from 'react'
+
+function usercard() {
+  return (
+    
+      <div className="user-card">
+      <img 
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzctuTZ5G01tvtrJwnQusRn6BUvK_4i0rtVg&s" 
+        alt="User Avatar" 
+      />
+      <h2>Name: Santhosh</h2>
+      <p>Email: santhosh###@gmail.com</p>
+      <p>Contact: 908008612</p>
+      <p>Address: Alliance Uni</p>
+    </div>
+  )
+}
+
+export default usercard
